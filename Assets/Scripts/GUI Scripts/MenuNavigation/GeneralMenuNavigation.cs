733cda847bae558fdc3ebdeb9e1a01bb779e3146
Ian Macleish <Ian.Macleish@akqa.com>
2016-01-29 15:36:17 -0800

    BasicMenuNavigation
    Implemented basic menu navigation for the main menu and all secondary
    scenes. This uses GeneralMenuNavigation.cs

---
+++ Assets/Scripts/GUI Scripts/MenuNavigation/GeneralMenuNavigation.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GeneralMenuNavigation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void LoadSceneMainMenu(){
+		SceneManager.LoadScene ("MainMenu");
+	}
+
+	public void LoadSceneLoadGame(){
+		SceneManager.LoadScene ("LoadGame");
+	}
+
+	public void LoadSceneNewGame(){
+		SceneManager.LoadScene ("NewGame");
+	}
+
+	public void LoadSceneSceneSelect(){
+		SceneManager.LoadScene ("SceneSelect");
+	}
+
+	public void LoadSceneOptions(){
+		SceneManager.LoadScene ("Options");
+	}
+
+	public void LoadSceneCredits(){
+		SceneManager.LoadScene ("Credits");
+	}
+
+	public void LoadSceneHighScores(){
+		SceneManager.LoadScene ("HighScores");
+	}
+
+}
